fd49948889a7b5df8131c0e6d90516bd39754641
tarasiki <td994@ukr.net>
2022-07-10 13:22:27 +0300

    Implement PersonalInfoPage

--- EasyRestProjectNetTeam2/EasyRestPages/PersonalInfoPage.cs
+++ EasyRestProjectNetTeam2/EasyRestPages/PersonalInfoPage.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,45 @@ namespace EasyRestProjectNetTeam2.EasyRestPages
     class PersonalInfoPage : BasePage
     {
         public PersonalInfoPage(IWebDriver driver) : base(driver)
+        { }
+        [FindsBy(How = How.XPath, Using = "//span [contains(@class, 'MuiButtonBase-root')]")]
+        private IWebElement _editPhoto;
+
+        [FindsBy(How = How.XPath, Using = "//span[(text()= 'Personal Info')]")]
+        private IWebElement _personalInfoButton;
+
+        [FindsBy(How = How.XPath, Using = "//span[(text()= 'Current Orders')]")]
+        private IWebElement _currentOrdersButton;
+
+        [FindsBy(How = How.XPath, Using = "//span[(text()= 'Order History')]")]
+        private IWebElement _orderhistoryButton;
+
+        [FindsBy(How = How.XPath, Using = "//span[(text()= 'My Restaurants')]")]
+        private IWebElement _myRestaurantsButton;
+
+        public void ClickEditPhoto()
         {
+            _editPhoto.Click();
+
+        }
+        public void ClickPersonalInfoButton()
+        {
+            _personalInfoButton.Click();
+
+        }
+        public void ClickCurrentOrdersButton()
+        {
+            _currentOrdersButton.Click();
+
+        }
+        public void ClickOrderHistoryButton()
+        {
+            _orderhistoryButton.Click();
+
+        }
+        public void MyRestaurantsButton()
+        {
+           _myRestaurantsButton.Click();    
 
         }
     }
