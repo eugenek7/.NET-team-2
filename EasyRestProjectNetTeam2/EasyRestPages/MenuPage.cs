f14b7dec351d9a5a3c193afe77ac9dac27a3c7b7
eugenek7 <kokhan.ye.yu@gmail.com>
2022-07-14 21:19:29 +0300

    Add web elements for MenuPage

--- EasyRestProjectNetTeam2/EasyRestPages/MenuPage.cs
+++ EasyRestProjectNetTeam2/EasyRestPages/MenuPage.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
 
 namespace EasyRestProjectNetTeam2.EasyRestPages
 {
@@ -8,5 +9,23 @@ namespace EasyRestProjectNetTeam2.EasyRestPages
         {
 
         }
+        [FindsBy(How = How.XPath, Using = "(//input[@id='quantity'])[1]")]
+        private IWebElement _inputItemQuantity1;
+        [FindsBy(How = How.XPath, Using = "(//input[@id='quantity'])[2]")]
+        private IWebElement _inputItemQuantity2;
+        [FindsBy(How = How.XPath, Using = "(//button[@aria-label='Add to cart'])[1]")]
+        private IWebElement _addToCartButton1;
+        [FindsBy(How = How.XPath, Using = "(//button[@aria-label='Add to cart'])[2]")]
+        private IWebElement _addToCartButton2;
+        [FindsBy(How = How.XPath, Using = "(//button[@aria-label='Remove item'])[1]")]
+        private IWebElement _removeItemButton;
+        [FindsBy(How = How.XPath, Using = "//span[text()='Submit order']/parent::button")]
+        private IWebElement _submitOrderButton;
+        [FindsBy(How = How.XPath, Using = "//span[text()='Submit']/parent::button")]
+        private IWebElement _submitButton;
+        [FindsBy(How = How.XPath, Using = "//label[text()='Date picker']/following-sibling::div/input")]
+        private IWebElement _inputDate;
+        [FindsBy(How = How.XPath, Using = "//label[text()='Time picker']/following-sibling::div/input")]
+        private IWebElement _inputTime;
     }
 }
