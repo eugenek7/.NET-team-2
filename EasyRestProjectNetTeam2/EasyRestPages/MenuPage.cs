cc33ed7505b257d29dc994075a6ec2f2beacc88d
eugenek7 <kokhan.ye.yu@gmail.com>
2022-07-19 16:50:07 +0300

    Add functions to MenuPage

--- EasyRestProjectNetTeam2/EasyRestPages/MenuPage.cs
+++ EasyRestProjectNetTeam2/EasyRestPages/MenuPage.cs
@@ -27,5 +27,53 @@ namespace EasyRestProjectNetTeam2.EasyRestPages
         private IWebElement _inputDate;
         [FindsBy(How = How.XPath, Using = "//label[text()='Time picker']/following-sibling::div/input")]
         private IWebElement _inputTime;
+
+        public void SendKeysToInputItemQuantity1(string quantity)
+        {
+            _inputItemQuantity1.SendKeys(quantity);
+
+        }
+        public void SendKeysToInputItemQuantity2(string quantity)
+        {
+            _inputItemQuantity2.SendKeys(quantity);
+
+        }
+        public void IncreaseItemQuantity()
+        {
+            _inputItemQuantity1.SendKeys(Keys.ArrowUp);
+        }
+        public void DecreaseItemQuantity()
+        {
+            _inputItemQuantity1.SendKeys(Keys.ArrowDown);
+        }
+        public void ClickAddToCartButton1()
+        {
+            _addToCartButton1.Click();
+        }
+        public void ClickAddToCartButton2()
+        {
+            _addToCartButton2.Click();
+        }
+        public void ClickRemoveItemButton() //removes first item from cart
+        {
+            _removeItemButton.Click();
+        }
+        public void ClickSubmitOrderButton() //opens Order confirmation pop-up
+        {
+            _submitOrderButton.Click();
+        }
+        public void ClickSubmitButton() //click submit button on Order confirmation pop-up
+        {
+            _submitButton.Click();
+        }
+        public void ClickOnDataPicker()
+        {
+            _inputDate.Click();
+        }
+        public void ClickOnTimePicker()
+        {
+            _inputTime.Click();
+        }
+
     }
 }
