8bdb2b0f14ee90114e9c5552e96a5297c217b6c0
eugenek7 <kokhan.ye.yu@gmail.com>
2022-08-01 23:18:42 +0300

    implement DatabaseManager.SendQuery method to work with SELECT

--- EasyRestProjectNetTeam2/Helpers/DatabaseManager.cs
+++ EasyRestProjectNetTeam2/Helpers/DatabaseManager.cs
@@ -16,9 +16,29 @@ namespace EasyRestProjectNetTeam2.Helpers
                 con.Close();
             }
         }
+
+        public static string SendQuery(string query, string queryVariable)
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                string formatQuery = String.Format(query, queryVariable);
+                NpgsqlCommand cmd = new NpgsqlCommand(formatQuery, con);
+                con.Open();
+                string result = string.Empty;
+                var getValue = cmd.ExecuteScalar();
+                if (getValue != null)
+                {
+                    result = getValue.ToString();
+                }
+                con.Close();
+                return result;
+            }
+        }
+
         private static NpgsqlConnection GetConnection()
         {
             return new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=admin;Password=12345678;database=easyrest;");
         }
+
     }
 }
--- EasyRestProjectNetTeam2/Models/QueryDataModel.cs
+++ EasyRestProjectNetTeam2/Models/QueryDataModel.cs
@@ -3,7 +3,7 @@
     public class QueryDataModel
     {
         public string DeleteUserByEmail { get; set; }
-
+        public string SelectUserEmailByEmail { get; set; }
 
     }
 }
