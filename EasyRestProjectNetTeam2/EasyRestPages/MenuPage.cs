efa39ff4c555aa1e6ff9ac081b92ea019e5b3ff2
eugenek7 <kokhan.ye.yu@gmail.com>
2022-08-03 15:16:00 +0300

    add private setters for components

--- EasyRestProjectNetTeam2/EasyRestPages/MenuPage.cs
+++ EasyRestProjectNetTeam2/EasyRestPages/MenuPage.cs
@@ -6,8 +6,8 @@ namespace EasyRestProjectNetTeam2.EasyRestPages
 {
     public class MenuPage : BasePage
     {
-        public DatePickerComponent DatePickerComponent { get; }
-        public TimePickerComponent TimePickerComponent { get; }
+        public DatePickerComponent DatePickerComponent { get; private set; }
+        public TimePickerComponent TimePickerComponent { get; private set; }
 
         public MenuPage(IWebDriver driver) : base(driver)
         {
