331d3a3398e3b8b39ffff08c8d244acde0b38d81
eugenek7 <kokhan.ye.yu@gmail.com>
2022-08-08 19:26:33 +0300

    Remove elements and methods, covered by components

--- EasyRestProjectNetTeam2/EasyRestPages/MenuPage.cs
+++ EasyRestProjectNetTeam2/EasyRestPages/MenuPage.cs
@@ -7,8 +7,6 @@ namespace EasyRestProjectNetTeam2.EasyRestPages
 {
     public class MenuPage : BasePage
     {
-        //public DatePickerComponent DatePickerComponent { get; private set; }
-        //public TimePickerComponent TimePickerComponent { get; private set; }
         public MenuOrderItemsListComponent MenuOrderItemsListComponent { get; private set; }
 
         public MenuPage(IWebDriver driver) : base(driver)
@@ -20,21 +18,6 @@ namespace EasyRestProjectNetTeam2.EasyRestPages
 
         [FindsBy(How = How.XPath, Using = "(//button[@aria-label='Add to cart'][not(@disabled)])[1]")]
         private IWebElement _addToCartButton;
-
-        //[FindsBy(How = How.XPath, Using = "(//button[@aria-label='Remove item'])[1]")]
-        //private IWebElement _removeItemButton;
-
-        //[FindsBy(How = How.XPath, Using = "//span[text()='Submit order']/parent::button")]
-        //private IWebElement _submitOrderButton;
-
-        //[FindsBy(How = How.XPath, Using = "//span[text()='Submit']/parent::button")]
-        //private IWebElement _submitButton;
-
-        //[FindsBy(How = How.XPath, Using = "//label[text()='Date picker']/following-sibling::div/input")]
-        //private IWebElement _inputDate;
-
-        //[FindsBy(How = How.XPath, Using = "//label[text()='Time picker']/following-sibling::div/input")]
-        //private IWebElement _inputTime;
 
         [FindsBy(How = How.XPath, Using = "//p[text()='Sorry, you can`t pick past book time']")]
         private IWebElement _errorPopUp;
@@ -69,33 +52,11 @@ namespace EasyRestProjectNetTeam2.EasyRestPages
             _inputItemQuantity.GetAttribute("value");
         }
 
-        public void ClickAddToCartButton()
-        {
-            _addToCartButton.Click();
-            MenuOrderItemsListComponent = new MenuOrderItemsListComponent(driver);
-        }
-
         public void WaitAndClickAddToCartButton(int TimeToWait)
         {
             _addToCartButton.WaitAndClick(driver, TimeToWait);
             MenuOrderItemsListComponent = new MenuOrderItemsListComponent(driver);
         }
-
-        //public void ClickRemoveItemButton() //removes first item from cart
-        //{
-        //    _removeItemButton.Click();
-        //}
-
-        //public void ClickSubmitOrderButton() //opens Order confirmation pop-up
-        //{
-        //    _submitOrderButton.Click();
-        //}
-
-        //public void ClickSubmitButton() //click submit button on Order confirmation pop-up
-        //{
-        //    _submitButton.Click();
-        //}
-
 
         public void ClikHotCatagory()
         {
@@ -126,18 +87,6 @@ namespace EasyRestProjectNetTeam2.EasyRestPages
         {
             WaitElementIsClickable(timeToWait, _coctailsCatagoryButton);
         }
-
-        //public void ClickOnDatePicker()
-        //{
-        //    _inputDate.Click();
-        //    DatePickerComponent = new DatePickerComponent(driver);
-        //}
-
-        //public void ClickOnTimePicker()
-        //{
-        //    _inputTime.Click();
-        //    TimePickerComponent = new TimePickerComponent(driver);
-        //}
 
         public string GetErrorPopupText()
         {
