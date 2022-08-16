180e34066ada997f11023e33be45694a1b44523e
Slim1011 <angidrit1011@gmail.com>
2022-08-16 19:07:12 +0300

    Added new project with ScreenPlay pattern
    Added new project with ScreenPlay pattern, necessary pages and tests for sign in user and moderator management

---
+++ EasyRestProjectScreenPlayPattern/Interactions/Questions/KeepInMind.cs
@@ -0,0 +1,21 @@
+﻿using Boa.Constrictor.Screenplay;
+using Boa.Constrictor.WebDriver;
+
+namespace BoaConstrictorTestProject.Interactions.Questions
+{
+    public class KeepInMind : IQuestion<string>
+    {
+        public IWebLocator Locator { get; }
+
+        private KeepInMind(IWebLocator locator) => Locator = locator;
+
+        public static KeepInMind NameOfFirstUserFrom(IWebLocator locator) => new KeepInMind(locator);
+        public static KeepInMind NameOfFirstOwnerFrom(IWebLocator locator) => new KeepInMind(locator);
+
+        public string RequestAs(IActor actor)
+        {
+            string firstStringFromList = actor.AskingFor(Remember.AllInfoFrom(Locator)).First();
+            return firstStringFromList;
+        }
+    }
+}
---
+++ EasyRestProjectScreenPlayPattern/Interactions/Tasks/SignIn.cs
@@ -0,0 +1,30 @@
+﻿using Boa.Constrictor.Screenplay;
+using Boa.Constrictor.WebDriver;
+using BoaConstrictorTestProject.Components;
+using BoaConstrictorTestProject.Pages;
+
+namespace BoaConstrictorTestProject.Interactions.Tasks
+{
+    public class SignIn : ITask
+    {
+        public string Email { get; }
+        public string Password { get; }
+
+        private SignIn(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+        public static SignIn WithEmailAndPassword(string email, string password)
+            => new SignIn(email, password);
+
+        public void PerformAs(IActor actor)
+        {
+            actor.AttemptsTo(Click.On(HeaderMenu.SignInButton));
+            actor.AttemptsTo(SendKeys.To(SignInPage.InputEmail, Email));
+            actor.AttemptsTo(SendKeys.To(SignInPage.InputPassword, Password));
+            actor.AttemptsTo(Click.On(SignInPage.SignInButton));
+        }
+    }
+}
