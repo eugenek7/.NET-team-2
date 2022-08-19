4211a0df42655664351a0d5e46ce38530825b8e5
Serhii_slm <serhiy.slm@gmail.com>
2022-08-19 09:44:15 +0300

    add

---
+++ EasyRestProjectNetTeam2/FacadeClass/FacadeClass.cs
@@ -0,0 +1,31 @@
+﻿using EasyRestProjectNetTeam2.EasyRestPages;
+using OpenQA.Selenium;
+
+namespace EasyRestProjectNetTeam2.FacadeClass
+{
+    public class FacadeClass
+    {
+        ManageMenuPage manageMenuPage;
+        ManageRestaurantsPage manageRestaurantsPage;
+
+        public NavigationFacade(IWebDriver driver)
+        {
+            manageMenuPage = new ManageMenuPage(driver);
+            manageRestaurantsPage = new ManageRestaurantsPage(driver);
+        }
+
+        public NavagationFacade NavigationToManageRestaurant(int timeToWait)
+        {
+            manageRestaurantsPage = GetManageRestaurantsPage();
+            manageRestaurantsPage.WaitAndClickMoreButton(timeToWait);
+            manageRestaurantsPage.WaitAndClickManageButton(timeToWait);
+            manageMenuPage = GetManageMenuPage();
+
+
+
+        }
+
+
+
+    }
+}
