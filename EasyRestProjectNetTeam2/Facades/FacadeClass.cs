081c312858a3ff331964bb41a2fc66f126336c3c
Serhii_slm <serhiy.slm@gmail.com>
2022-08-22 09:41:36 +0300

    Update FacadeClass.cs

--- EasyRestProjectNetTeam2/Facades/FacadeClass.cs
+++ EasyRestProjectNetTeam2/Facades/FacadeClass.cs
@@ -2,25 +2,25 @@
 
 namespace EasyRestProjectNetTeam2.FacadeClass
 {
-    public class NavigateTo
+    public class NavigateToManageRestaurantFacade
     {
         private readonly ManageMenuPage manageMenuPage;
         private readonly ManageRestaurantsPage manageRestaurantsPage;
 
-        public NavigateTo(ManageMenuPage manageMenuPage, ManageRestaurantsPage manageRestaurantsPage)
+        public NavigateToManageRestaurantFacade(ManageMenuPage manageMenuPage, ManageRestaurantsPage manageRestaurantsPage)
         {
             this.manageMenuPage = manageMenuPage;
             this.manageRestaurantsPage = manageRestaurantsPage;
         }
 
-        public void ManageWaitersPage(int timeToWait)
+        public void NavigateToManageWaitersPage(int timeToWait)
         {
             manageRestaurantsPage.WaitAndClickMoreButton(timeToWait);
             manageRestaurantsPage.WaitAndClickManageButton(timeToWait);
             manageMenuPage.LeftBarComponent.WaitAndClickWaiterLeftBarButton(timeToWait);
         }
 
-        public void ManageAdministratorPage(int timeToWait)
+        public void NavigateToManageAdministratorPage(int timeToWait)
         {
             manageRestaurantsPage.WaitAndClickMoreButton(timeToWait);
             manageRestaurantsPage.WaitAndClickManageButton(timeToWait);
