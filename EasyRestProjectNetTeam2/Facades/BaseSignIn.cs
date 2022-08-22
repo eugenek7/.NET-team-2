fdc745fdc5ce578dd68bbff88782c73fe70fd733
Ahnessa Pleshchytser <89419679+AhnessP@users.noreply.github.com>
2022-08-22 10:52:16 +0300

    add facade for signIn (#51)

---
+++ EasyRestProjectNetTeam2/Facades/BaseSignIn.cs
@@ -0,0 +1,25 @@
+using EasyRestProjectNetTeam2.EasyRestPages;
+using OpenQA.Selenium;
+
+namespace EasyRestProjectNetTeam2.Facades
+{
+    public class BaseSignIn
+    {
+        private readonly  SignInPage signInPage;
+        private readonly HomePage homePage;
+
+        public BaseSignIn(SignInPage signInPage, HomePage homePage)
+        {
+            this.signInPage = signInPage;
+            this.homePage = homePage;
+        }
+
+        public void  SignIn(string Email, string Password)
+        {  
+            homePage.HeaderMenuComponent.ClickSignInButton();
+            signInPage.SendKeysToInputEmail(Email);
+            signInPage.SendKeysToInputPassword(Password);
+            signInPage.HeaderMenuComponent.ClickSignInButton();
+        }
+    }
+}
