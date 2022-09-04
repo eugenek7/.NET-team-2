93f6c580fc757cb93ec1abead61d9b97710e1f93
Slim1011 <angidrit1011@gmail.com>
2022-09-04 12:20:16 +0300

    refactor names of methods from steps definitions
    refactor names of methods from steps definitions

--- EasyRestPlaywrightSpecflowProject/StepDefinitions/EasyRestModeratorTestsStepDefinitions.cs
+++ EasyRestPlaywrightSpecflowProject/StepDefinitions/EasyRestModeratorTestsStepDefinitions.cs
@@ -22,14 +22,14 @@ namespace EasyRestPlaywrightSpecflowProject.StepDefinitions
 
 
         [Given(@"I go to Easy rest page")]
-        public void GivenIGoToEasyRestPage()
+        public void NavigateToEasyRestPage()
         {
             _driver.Page.GotoAsync(_url);
             _homePage = new HomePage(_driver.Page);
         }
 
         [Given(@"I log in to my account with '([^']*)' and '([^']*)'")]
-        public async Task GivenILogInToMyAccountWithAnd(string email, string password)
+        public async Task LogInToAccountWithEmailAndPassword(string email, string password)
         {
             await _homePage.ClickSignInButton();
             _signInPage = new SignInPage(_driver.Page);
@@ -39,14 +39,14 @@ namespace EasyRestPlaywrightSpecflowProject.StepDefinitions
         }
 
         [Given(@"I go to Users tab")]
-        public async Task GivenIGoToUsersTab()
+        public async Task NavigateToUsersTab()
         {
             _moderatorManagePage = new ModeratorManagePage(_driver.Page);
             await _moderatorManagePage.ClickUsersButton();
         }
 
         [Given(@"I go to Owners tab")]
-        public async void GivenIGoToOwnersTab()
+        public async void NavigateToOwnersTab()
         {
             _moderatorManagePage = new ModeratorManagePage(_driver.Page);
             await _moderatorManagePage.ClickOwnersButton();
@@ -54,7 +54,7 @@ namespace EasyRestPlaywrightSpecflowProject.StepDefinitions
 
         [Given(@"I go to Active tab")]
         [When(@"I go to Active tab")]
-        public async Task GivenIGoToActiveTab()
+        public async Task NavigateToActiveTab()
         {
             await _moderatorManagePage.ClickActiveButton();
         }
@@ -63,7 +63,7 @@ namespace EasyRestPlaywrightSpecflowProject.StepDefinitions
         [When(@"I unban first active user")]
         [When(@"I ban first active owner")]
         [When(@"I unban first active owner")]
-        public async Task WhenIBanFirstActiveUser()
+        public async Task RememberNameOfFirstPersonAndBanFirstPerson()
         {
             var personsNames = await _moderatorManagePage.GetAllNames();
             firstName = personsNames[0];
@@ -72,7 +72,7 @@ namespace EasyRestPlaywrightSpecflowProject.StepDefinitions
 
         [Given(@"I go to Banned tab")]
         [When(@"I go to Banned tab")]
-        public async Task WhenIGoToBannedTab()
+        public async Task NavigateToBannedTab()
         {
             await _moderatorManagePage.ClickBannedButton();
         }
@@ -81,7 +81,7 @@ namespace EasyRestPlaywrightSpecflowProject.StepDefinitions
         [Then(@"I check that same user appears in active tab")]
         [Then(@"I check that same owner appears in banned tab")]
         [Then(@"I check that same owner appears in active tab")]
-        public async Task ThenICheckThatSameUserAppearsInBannedTab()
+        public async Task CheckThatSamePersonAppearsInCorrectTab()
         {
             var personsNames = await _moderatorManagePage.GetAllNames();
             bool isFirsrtUserExist = await _moderatorManagePage.IsSearchWordPresentInList(personsNames, firstName);
